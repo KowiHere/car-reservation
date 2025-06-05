26765f888a1792eebafc499293516538a0c4cdfb
Jakub <jkschool@wp.pl>
2025-06-05 23:33:51 +0200

    1st Run

---
+++ src/main/java/com/crp/warsztat/WarsztatApplication.java
@@ -0,0 +1,13 @@
+package com.crp.warsztat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarsztatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crp/warsztat/WarsztatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crp.warsztat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsztatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
